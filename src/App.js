d779694bc3cec63ed68ed1d7d9f10a3fe598bb88
Guillermo F Greco <guillermogreco@fb.com>
2020-10-28 15:32:31 -0700

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class App extends Component{
+import {CardList} from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+import './components/title-font/title-font.styles.css'
+class App extends React.Component{
   constructor(){
     super();
-
     this.state ={
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        },
-      ]
+      monsters: [],
+      searchField: '',
     };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
   }
 
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
-    <div className="App">
-
-    </div>
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='Search monsters...'
+          handleChange= {e => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters}>
+        </CardList>
+      </div>
     );
   }
 }
